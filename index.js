c81d7816417f873f34dda863e06c2c29386dc2a6
mhh01425734 <1326303027@qq.com>
2024-01-08 17:33:16 +0800

    My third action is ready

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+const axios = require('axios');
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  core.setOutput("name", "mhh");
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    // `who-to-greet` input defined in action metadata file
+    const requestUrl = core.getInput('requestUrl');
+    // 发送GET请求
+    const response = await axios.get(requestUrl);
+    const {branches_tags, date} = response.data;
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("date", date);
+    core.setOutput("branches_tags", branches_tags);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
