994ec97160cc79ed545ec27847133914edd5d514
mhh01425734 <1326303027@qq.com>
2024-01-08 17:37:29 +0800

    My third action is ready

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
     // `who-to-greet` input defined in action metadata file
     const requestUrl = core.getInput('requestUrl');
     // 发送GET请求
-    const response = await axios.get(requestUrl);
+    const response = axios.get(requestUrl);
     const {branches_tags, date} = response.data;
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
