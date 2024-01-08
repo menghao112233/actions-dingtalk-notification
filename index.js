5537e592569f9e5ecdbaf0f266b394ef797f7d78
mhh01425734 <1326303027@qq.com>
2024-01-08 18:09:10 +0800

    My 5 action is ready

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const github = require('@actions/github');
 const axios = require('axios');
 try {
     // `who-to-greet` input defined in action metadata file
-    const requestUrl = core.getInput('requestUrl');
+    // const requestUrl = core.getInput(requestUrl);
     // 发送GET请求
-    const response = axios.get(requestUrl);
-    const {branches_tags, date} = response.data;
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("date", date);
-    core.setOutput("branches_tags", branches_tags);
+    axios.get("https://beta.oscollege.net/api/version").then(response => {
+        const {version, date} = response.data;
+        core.setOutput("date", date);
+        core.setOutput("branches_tags", version);
+        console.log(date)
+        console.log(version)
+    }).catch(reason => {
+        console.error('Promise rejected with reason:', reason);
+    });
+
     // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2)
     // console.log(`The event payload: ${payload}`);
