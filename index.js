d4afb825b8b100e44cb781bafbae1434c2a6c848
mhh01425734 <1326303027@qq.com>
2024-01-10 10:37:09 +0800

    My 21 action is ready

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const success_start_text =
 let markdown_text = "";
 const end_text =
     `#### 发布人: ${github.context.actor} \n ` +
-    core.getInput("rich_text")
+    core.getInput("footer_text")
 const markdown_data = {
     msgtype: "markdown",
     markdown: {
@@ -30,14 +30,14 @@ const fail_start_text =
     ` <font color=Orange>请先查看[发布版本](${requestUrl})是否误报,再查询问题</font>\n` +
     "|                 | <font color=Darkorange>**before**</font>  | <font color=Green>**after**</font>                  |\n" +
     "| --------------: | ------------- | ---------------------------- |\n";
-let dataJson = core.getInput("data_json");
-if (!dataJson) {
+let old_json = core.getInput("old_json");
+if (!old_json) {
     try {
         // `who-to-greet` input defined in action metadata file
         // 发送GET请求
         axios.get(requestUrl).then(response => {
             const data = response.data;
-            core.setOutput("data_json", data);
+            core.setOutput("old_json", data);
             console.log("axios.get request_url的值: " + JSON.stringify(data))
 
         }).catch(reason => {
@@ -59,14 +59,14 @@ if (!dataJson) {
             const data = response.data;
             let flag = true;
             const flag_data = JSON.parse(core.getInput("flag_data"));
-            dataJson = JSON.parse(dataJson)
+            old_json = JSON.parse(old_json)
             for (const item in data) {
 
                 if (!(flag_data[item] === data[item]) && flag) {
                     flag = false;
                 }
 
-                const before_value = dataJson[item];
+                const before_value = old_json[item];
                 const after_value = data[item];
                 markdown_text += `| <small>**${item}:**</small>    | <small><font color=Darkorange>${before_value}<font></small>          |<small><font color=Green> ${after_value} <font></small>                     |\n`;
 
