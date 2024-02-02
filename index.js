fa6a9da7bbd1dd3ff5a6b8926f96af215457893a
mhh01425734 <1326303027@qq.com>
2024-02-02 15:59:32 +0800

    打印日志测试

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const core = require('@actions/core');
-const axios = require('axios');
-
-
-console.log(${process.env.GITHUB_REPOSITORY})
-
-console.log(${process.env.GITHUB_ACTOR})
+console.log(`${process.env.GITHUB_REPOSITORY}`);
+console.log(`${process.env.GITHUB_ACTOR}`);
