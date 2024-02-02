02dd5f49fa63a482a7c08340d2cbed23cedf7f43
mhh01425734 <1326303027@qq.com>
2024-02-02 14:50:27 +0800

    打印日志测试

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const axios = require('axios');
 
 console.log("start")
 // const requestUrl = core.getInput("request_url");
