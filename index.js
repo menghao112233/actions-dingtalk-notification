67175a12b01c4bc79254c29c33338236d59c75c4
mhh01425734 <1326303027@qq.com>
2024-02-02 14:48:31 +0800

    打印日志测试

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const github = require('@actions/github');
 const axios = require('axios');
 
 console.log("start")
-const requestUrl = core.getInput("request_url");
-console.log(requestUrl)
+// const requestUrl = core.getInput("request_url");
+// console.log(requestUrl)
 
 // const old_json = core.getInput("old_json");
 console.log(1)
