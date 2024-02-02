5dbf3e6ba3ba73297bc5287a3f27b97c0114bf52
mhh01425734 <1326303027@qq.com>
2024-02-02 14:46:44 +0800

    打印日志测试

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ const github = require('@actions/github');
 const axios = require('axios');
 
 console.log("start")
-    // const requestUrl = core.getInput("request_url");
-    console.log(requestUrl)
+const requestUrl = core.getInput("request_url");
+console.log(requestUrl)
 
-    // const old_json = core.getInput("old_json");
-    console.log(1)
-    if (!old_json) {
-        console.log(2)
-        try {
-            console.log("requestUrl:" + requestUrl)
+// const old_json = core.getInput("old_json");
+console.log(1)
 
-        } catch (error) {
-            core.setFailed(error.message);
-        }
-    }
