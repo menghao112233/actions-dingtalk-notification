cc3aee7ed3ceca82097d87953f13c4ecb70a81f4
mhh01425734 <1326303027@qq.com>
2024-02-06 10:42:07 +0800

    拆分

--- api/axios.js
+++ api/axios.js
@@ -8,19 +8,30 @@ const failDingTalkAxios = () => {
 
 }
 
-function requestUrlAxios(requestUrl) {
+function beforeRequestUrlAxios(requestUrl) {
     axios.get(requestUrl).then(response => {
         const data = response.data;
         console.log("axios.get request_url的值: " + JSON.stringify(data))
-        // process.env.BEFORE_DATA = JSON.stringify(data);
         fs.writeFileSync(process.env.GITHUB_ENV, `BEFORE_DATA=${JSON.stringify(data)}`);
-
     }).catch(reason => {
         console.error('Promise rejected with reason:', reason);
         return null;
     });
 }
 
+function afterRequestUrlAxios(requestUrl) {
+    axios.get(requestUrl).then(response => {
+        const data = response.data;
+        console.log("axios.get request_url的值: " + JSON.stringify(data))
+        process.env.AFTER_DATA=JSON.stringify(data);
+    }).catch(reason => {
+        console.error('Promise rejected with reason:', reason);
+        return null;
+    });
+}
+
+
 module.exports = {
-    requestUrlAxios
+    beforeRequestUrlAxios,
+    afterRequestUrlAxios
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {fail_title, success_start_text, end_text, markdown_data, fail_start_text} = require('./constants/dingTalk');
-const {requestUrlAxios} = require('./api/axios')
+const {beforeRequestUrlAxios,afterRequestUrlAxios} = require('./api/axios')
 const core = require('@actions/core');
 const axios = require('axios');
 
@@ -10,13 +10,13 @@ let markdown_text = "";
 const requestUrl = core.getInput("request_url");
 
 
-let old_json = process.env.BEFORE_DATA
-console.log("old_json: -->" + old_json)
-if (!old_json) {
+let before_data = process.env.BEFORE_DATA
+console.log("before_data: -->" + before_data)
+if (!before_data) {
     try {
         // `who-to-greet` input defined in action metadata file
         // 发送GET请求
-        requestUrlAxios(requestUrl)
+        beforeRequestUrlAxios(requestUrl)
     } catch (error) {
         core.setFailed(error.message);
     }
@@ -27,51 +27,76 @@ if (!old_json) {
         // `who-to-greet` input defined in action metadata file
         // const requestUrl = core.getInput(requestUrl);
         // 发送GET请求
-        axios.get(requestUrl).then(response => {
-            const data = response.data;
-            let flag = true;
-            const flag_data = JSON.parse(core.getInput("flag_data"));
-            console.log("flag_data的值:  " + JSON.stringify(flag_data))
-            console.log("old_json的值:  " + old_json)
-            console.log("当前data的值:  " + JSON.stringify(data))
-            old_json = JSON.parse(old_json)
-            for (const key in flag_data) {
-                if (!(flag_data[key] === data[key]) && flag) {
-                    flag = false;
-                }
-            }
-            for (const item in data) {
+        afterRequestUrlAxios(requestUrl);
 
-                const before_value = old_json[item];
-                const after_value = data[item];
-                markdown_text += `| <small>**${item}:**</small>    | <small><font color=Darkorange>${before_value}<font></small>          |<small><font color=Green> ${after_value} <font></small>                     |\n`;
+        for (const item in process.env.AFTER_DATA) {
 
-            }
-            if (flag) {
-                markdown_data.markdown.text =
-                    success_start_text +
-                    markdown_text +
-                    end_text;
-            } else {
-                markdown_data.markdown.title = fail_title
-                markdown_data.markdown.text =
-                    fail_start_text +
-                    markdown_text +
-                    end_text;
-            }
+            const before_value = before_data[item];
+            const after_value = process.env.AFTER_DATA[item];
+            markdown_text += `| <small>**${item}:**</small>    | <small><font color=Darkorange>${before_value}<font></small>          |<small><font color=Green> ${after_value} <font></small>                     |\n`;
 
-            axios.post(
-                core.getInput("ding_talk_url"),
-                markdown_data,
-            ).then((response) => {
-                console.log('DingTalk message sent successfully:  ' + JSON.stringify(`${response.data}`));
-            }).catch((error) => {
-                console.error('Error sending DingTalk message:', error);
-            });
+        }
+        markdown_data.markdown.text =
+            success_start_text +
+            markdown_text +
+            end_text;
 
-        }).catch(reason => {
-            console.error('Promise rejected with reason:', reason);
+        axios.post(
+            core.getInput("ding_talk_url"),
+            markdown_data,
+        ).then((response) => {
+            console.log('DingTalk message sent successfully:  ' + JSON.stringify(`${response.data}`));
+        }).catch((error) => {
+            console.error('Error sending DingTalk message:', error);
         });
+
+
+        // axios.get(requestUrl).then(response => {
+        //     const data = response.data;
+        //     let flag = true;
+        //     const flag_data = JSON.parse(core.getInput("flag_data"));
+        //     console.log("flag_data的值:  " + JSON.stringify(flag_data))
+        //     console.log("before_data的值:  " + before_data)
+        //     console.log("当前data的值:  " + JSON.stringify(data))
+        //     before_data = JSON.parse(before_data)
+        //     for (const key in flag_data) {
+        //         if (!(flag_data[key] === data[key]) && flag) {
+        //             flag = false;
+        //         }
+        //     }
+        //     for (const item in data) {
+        //
+        //         const before_value = before_data[item];
+        //         const after_value = data[item];
+        //         markdown_text += `| <small>**${item}:**</small>    | <small><font color=Darkorange>${before_value}<font></small>          |<small><font color=Green> ${after_value} <font></small>                     |\n`;
+        //
+        //     }
+        //     if (flag) {
+        //         markdown_data.markdown.text =
+        //             success_start_text +
+        //             markdown_text +
+        //             end_text;
+        //     } else {
+        //         markdown_data.markdown.title = fail_title
+        //         markdown_data.markdown.text =
+        //             fail_start_text +
+        //             markdown_text +
+        //             end_text;
+        //     }
+        //
+        //     axios.post(
+        //         core.getInput("ding_talk_url"),
+        //         markdown_data,
+        //     ).then((response) => {
+        //         console.log('DingTalk message sent successfully:  ' + JSON.stringify(`${response.data}`));
+        //     }).catch((error) => {
+        //         console.error('Error sending DingTalk message:', error);
+        //     });
+        //
+        // }
+        // ).catch(reason => {
+        //     console.error('Promise rejected with reason:', reason);
+        // });
         // Get the JSON webhook payload for the event that triggered the workflow
         // const payload = JSON.stringify(github.context.payload, undefined, 2)
         // console.log(`The event payload: ${payload}`);
