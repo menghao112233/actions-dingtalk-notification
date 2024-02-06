458c020a9c4e2f15f8112ea3c124baede272e35f
mhh01425734 <1326303027@qq.com>
2024-02-06 15:25:15 +0800

    拆分

--- index.js
+++ index.js
@@ -10,36 +10,41 @@ let markdown_text = "";
 async function main() {
 
     //校验验证参数
-    let check = core.getInput("check");
+    let validate = core.getInput("validate");
 
     //钉钉消息before内容
     let before_data = process.env.BEFORE_DATA
-    console.log("before_data: -->" + before_data)
+    console.log("before_data: " + before_data)
 
     //获取钉钉消息内容接口地址
-    const requestUrl = core.getInput("request_url");
+    let requestUrl = core.getInput("request_url");
 
     //根据before_data参数判断是获取before_data内容消息 还是 组装钉钉消息内容发送钉钉消息.
     if (!before_data) {
         // `who-to-greet` input defined in action metadata file
         // 发送GET请求
         const before_data = await requestUrlAxios(requestUrl)
-        console.log("before_data request_url的值: " + JSON.stringify(before_data))
+        console.log("before_data: " + JSON.stringify(before_data))
         //将消息内容添加到 变量中.
         fs.writeFileSync(process.env.GITHUB_ENV, `BEFORE_DATA=${JSON.stringify(before_data)}`);
+        //将请求地址存入添加到变量中 供下次直接使用
+        fs.writeFileSync(process.env.GITHUB_ENV, `REQUEST_URL=${requestUrl}`);
         return;
     }
+
+    requestUrl = process.env.REQUEST_URL
+    console.log("requestUrl: "+requestUrl)
     //进行校验
-    if (check) {
-        console.log("check: " + check);
-        check = JSON.parse(check)
+    if (validate) {
+        console.log("validate: " + validate);
+        validate = JSON.parse(validate)
         const after_data = await requestUrlAxios(requestUrl)
         before_data = JSON.parse(before_data)
-        console.log("after_data request_url的值: " + JSON.stringify(after_data))
+        console.log("after_data: " + JSON.stringify(after_data))
 
         let flag = true;
-        for (const key in check) {
-            if (!(check[key] === after_data[key]) && flag) {
+        for (const key in validate) {
+            if (!(validate[key] === after_data[key]) && flag) {
                 flag = false;
             }
         }
@@ -75,16 +80,12 @@ async function main() {
 
     const after_data = await requestUrlAxios(requestUrl)
     before_data = JSON.parse(before_data)
-    console.log("after_data request_url的值: " + JSON.stringify(after_data))
+    console.log("after_data: " + JSON.stringify(after_data))
 
     for (const item in after_data) {
-
         const before_value = before_data[item];
-        console.log("before_value:" + before_value)
         const after_value = after_data[item];
-        console.log("after_value:" + after_value)
         markdown_text += `| <small>**${item}:**</small>    | <small><font color=Darkorange>${before_value}<font></small>          |<small><font color=Green> ${after_value} <font></small>                     |\n`;
-
     }
     console.log("markdown_text:" + markdown_text)
     markdown_data.markdown.text =
