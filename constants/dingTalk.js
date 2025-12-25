3a78e82a13da46aaa0488ea78a228804644d3be4
menghao112233 <mhh01425734@antgroup.com>
2025-12-25 16:21:54 +0800

    钉钉通知 简易版

--- constants/dingTalk.js
+++ constants/dingTalk.js
@@ -7,7 +7,15 @@ const actor = process.env.GITHUB_ACTOR;
 // 仓库/项目名
 const repository = process.env.GITHUB_REPOSITORY;
 
-const requestUrl = core.getInput("request_url");
+// 版本号
+const gitHubRef = process.env.GITHUB_REF;
+
+// 域名
+const domain = process.env.DOMAIN;
+
+
+// 钉钉群地址
+const dingTalkUrl = core.getInput("ding_talk_url");
 
 
 //项目名
@@ -19,17 +27,16 @@ if (repository && repository.includes('/')) {
 
 const success_title = `github发布${projectName}完成`
 
-const fail_title = `github发布${projectName}失败`
 
 const success_start_text =
     `### <font color=Green>github发布${projectName}完成</font>\n` +
-    "|                 | <font color=Darkorange>**before**</font>  | <font color=Green>**after**</font>                  |\n" +
-    "| --------------: | ------------- | ---------------------------- |\n";
+    "|   <font color=Green>**项目地址**</font>  | <font color=Green>**发布人**</font>  | <font color=Green>**版本号**</font>                  |\n" +
+    "| -------------- | ------------- | ---------------------------- |\n";
 
 
 const end_text =
-    `#### 发布人: ${actor} \n ` +
-    core.getInput("footer_text")
+    `#### \n ` +
+    `[${domain}](${domain})  \n `
 
 const markdown_data = {
     msgtype: "markdown",
@@ -42,17 +49,8 @@ const markdown_data = {
     },
 };
 
-const fail_start_text =
-    `### <font color=Red>github发布${projectName}未通过</font>\n` +
-    ` <font color=Orange>请先查看[发布版本](${process.env.REQUEST_URL})是否误报,再查询问题</font>\n` +
-    "|                 | <font color=Darkorange>**before**</font>  | <font color=Green>**after**</font>                  |\n" +
-    "| --------------: | ------------- | ---------------------------- |\n";
-
-
 module.exports = {
-    fail_title,
     success_start_text,
-    fail_start_text,
     end_text,
     markdown_data
 };
--- index.js
+++ index.js
@@ -1,168 +1,32 @@
 const fs = require('fs');
-const {fail_title, success_start_text, end_text, markdown_data, fail_start_text} = require('./constants/dingTalk');
-const {requestUrlAxios, dingTalkAxios} = require('./api/axios')
+const {
+    success_start_text,
+    repository,
+    end_text,
+    markdown_data,
+    projectName,
+    actor,
+    gitHubRef,
+    dingTalkUrl
+} = require('./constants/dingTalk');
+const { dingTalkAxios} = require('./api/axios')
 const core = require('@actions/core');
 
 //发送钉钉消息内容
 let markdown_text = "";
 
 
-//验证是否为有效的URL
-function isURL(str) {
-    // 匹配URL的模式
-    var pattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-    return pattern.test(str);
-}
-
-// 判断数据是否是 HTML 格式
-const regex = /<!doctype html>/i;
-
-//读取文件
-function readFileSync(html) {
-    let jsonString = "{}"
-    try {
-        // 读取文件的第一行内容
-        const firstLine = html.split('\n')[0];
-        console.log("firstLine:" + firstLine)
-        console.log(firstLine)
-        // 从注释中提取JSON字符串
-        const match = firstLine.match(/<!--(.*)-->/);
-        jsonString = match ? match[1] : '';
-        console.log("firstLine—jsonString:" + jsonString)
-    } catch (e) {
-        console.log(e)
-    }
-    return jsonString;
-}
-
 async function main() {
 
-    //校验验证参数
-    let validate = core.getInput("validate");
+    markdown_text = `| [${repository}](https://github.com/cloudlab-os/${projectName})  | ${actor} | ${gitHubRef} | 
+`
 
-    //钉钉消息before内容
-    let before_data = process.env.BEFORE_DATA
-    console.log("before_data: " + before_data)
-    //获取钉钉消息内容接口地址
-    let requestUrl = core.getInput("request_url");
-
-    //根据before_data参数判断是获取before_data内容消息 还是 组装钉钉消息内容发送钉钉消息.
-    if (!before_data) {
-
-        // 发送GET请求获取内容
-        before_data = await requestUrlAxios(requestUrl);
-        // 判断是否是html
-        if (regex.test(before_data)) {
-            //读取文件的第一行内容
-            before_data = readFileSync(before_data);
-        } else {
-            before_data = JSON.stringify(before_data)
-            console.log("before_data: " + before_data)
-        }
-
-        //将消息内容添加到 变量中.
-        fs.writeFileSync(process.env.GITHUB_ENV, `BEFORE_DATA=${before_data}`);
-        //将请求地址存入添加到变量中 供下次直接使用
-        fs.appendFileSync(process.env.GITHUB_ENV, `\nREQUEST_URL=${requestUrl}`);
-        return;
-    }
-    //之后数据
-    let after_data
-    //从变量中获取请求地址
-    requestUrl = process.env.REQUEST_URL
-    console.log("requestUrl: " + requestUrl)
-    //进行校验
-    if (validate) {
-        console.log("validate: " + validate);
-        //获取校验参数
-        validate = JSON.parse(validate)
-
-        //发送GET请求获取内容
-        after_data = await requestUrlAxios(requestUrl)
-
-        //判断请求地址是否为url
-        if (regex.test(after_data)) {
-            //读取文件的第一行内容
-            after_data = readFileSync(after_data)
-            after_data = JSON.parse(after_data)
-
-        }
-        console.log("after_data: " + JSON.stringify(after_data))
-        //将json字符串解析为Json
-        before_data = JSON.parse(before_data)
-
-
-        //判断校验成功失败标识
-        let flag = true;
-
-        //根据传入的校验值和查询到的数据进行比对
-        for (const key in validate) {
-            if (!(validate[key] === after_data[key]) && flag) {
-                flag = false;
-            }
-        }
-
-        //封装markdown_text
-        for (const item in after_data) {
-
-            const before_value = before_data[item];
-            console.log("before_value:" + before_value)
-            const after_value = after_data[item];
-            console.log("after_value:" + after_value)
-            markdown_text += `| <small>**${item}:**</small>    | <small><font color=Darkorange>${before_value}<font></small>          |<small><font color=Green> ${after_value} <font></small>                     |\n`;
-
-        }
-        console.log("markdown_text:" + markdown_text)
-
-        if (flag) {
-            //markdown_data 发送成功数据
-            markdown_data.markdown.text =
-                success_start_text +
-                markdown_text +
-                end_text;
-        } else {
-            //markdown_data 发送失败数据
-            markdown_data.markdown.title = fail_title
-            markdown_data.markdown.text =
-                fail_start_text +
-                markdown_text +
-                end_text;
-        }
-        //发送钉钉通知
-        dingTalkAxios(core.getInput("im_hook_url"), markdown_data)
-        return;
-    }
-
-    //这段代码是不进行校验 一直发送成功模板数据
-
-    //发送GET请求获取内容
-    after_data = await requestUrlAxios(requestUrl)
-
-    //判断请求地址是否为url
-    if (regex.test(after_data)) {
-        //读取文件的第一行内容
-        after_data = readFileSync(after_data)
-        after_data = JSON.parse(after_data)
-    }
-    console.log("after_data: " + JSON.stringify(after_data))
-    //将json字符串解析为Json
-    before_data = JSON.parse(before_data)
-
-    //封装markdown_text
-    for (const item in after_data) {
-        const before_value = before_data[item];
-        const after_value = after_data[item];
-        markdown_text += `| <small>**${item}:**</small>    | <small><font color=Darkorange>${before_value}<font></small>          |<small><font color=Green> ${after_value} <font></small>                     |\n`;
-    }
-
-    //markdown_data 发送成功数据
-    console.log("markdown_text:" + markdown_text)
     markdown_data.markdown.text =
         success_start_text +
         markdown_text +
         end_text;
     //发送钉钉通知
-    dingTalkAxios(core.getInput("im_hook_url"), markdown_data)
+    dingTalkAxios(core.getInput(dingTalkUrl), markdown_data)
 
 }
 
