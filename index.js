b9da927f6ce83774da1fda4baac66ce292477ab2
menghao112233 <mhh01425734@antgroup.com>
2025-12-25 16:55:30 +0800

    钉钉通知 简易版

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const {
     success_start_text,
-    repository,
     end_text,
     markdown_data,
     projectName,
 } = require('./constants/dingTalk');
-const { dingTalkAxios} = require('./api/axios')
+const {dingTalkAxios} = require('./api/axios')
 const core = require('@actions/core');
 
 //发送钉钉消息内容
@@ -19,13 +18,13 @@ const gitHubRef = process.env.GITHUB_REF;
 
 async function main() {
     
-    markdown_text = `| [${repository}](https://github.com/cloudlab-os/${projectName})  | ${actor} | ${gitHubRef} |`
+    markdown_text = `| [${projectName}](https://github.com/cloudlab-os/${projectName})  | ${actor} | ${gitHubRef} |\n`
 
     markdown_data.markdown.text =
         success_start_text +
         markdown_text +
         end_text;
-    
+
     //发送钉钉通知
     dingTalkAxios(core.getInput("ding_talk_url"), markdown_data)
 
